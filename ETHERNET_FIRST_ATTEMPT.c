2b531371e2130e3b4e559312ebdc29e8a1f22cc6
Maroua Bellatreche <62667666+Marouamx@users.noreply.github.com>
2020-12-21 10:14:55 +0100

    Update ETHERNET_FIRST_ATTEMPT.c

--- ETHERNET_FIRST_ATTEMPT.c
+++ ETHERNET_FIRST_ATTEMPT.c
@@ -2,6 +2,19 @@
 #include "wizchip_conf.h"
 #include "socket.h"
 #include <string.h>
+#include <stdio.h>
+
+#define _WIZCHIP_ W5500
+
+
+
+#define SOCK_TCPS       0
+#define SOCK_UDPS       1
+#define PORT_TCPS		5000
+#define PORT_UDPS       3000
+
+#define DATA_BUF_SIZE   2048
+
 
 #define SEPARATOR            "=============================================\r\n"
 #define WELCOME_MSG  		 "Welcome to STM32Nucleo Ethernet configuration\r\n"
@@ -17,15 +30,11 @@
 #define WRONG_STATUS_MSG	 "Something went wrong; STATUS: %d\r\n"
 #define LISTEN_ERR_MSG		 "LISTEN Error!\r\n"
 
-#define PRINT_STR(msg) do  {										\
-  HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);		\
+
+#define PRINT_STR(msg) do  {\
+  HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);\
 } while(0)
 
-#define PRINT_HEADER() do  {													\
-  HAL_UART_Transmit(&huart2, (uint8_t*)SEPARATOR, strlen(SEPARATOR), 100);		\
-  HAL_UART_Transmit(&huart2, (uint8_t*)WELCOME_MSG, strlen(WELCOME_MSG), 100);	\
-  HAL_UART_Transmit(&huart2, (uint8_t*)SEPARATOR, strlen(SEPARATOR), 100);		\
-} while(0)
 
 #define PRINT_NETINFO(netInfo) do { 																					\
   HAL_UART_Transmit(&huart2, (uint8_t*)NETWORK_MSG, strlen(NETWORK_MSG), 100);											\
@@ -39,8 +48,27 @@
   HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);															\
 } while(0)
 
+
+uint8_t gDATABUF[DATA_BUF_SIZE];
+
+
+
+wiz_NetInfo gWIZNETINFO = { .mac = {0x00, 0x08, 0xdc,0x00, 0xab, 0xcd},
+                            .ip = {192, 168, 1, 40},
+                            .sn = {255,255,255,0},
+                            .gw = {192, 168, 1, 1},
+                            .dns = {8,8,8,8},
+                            .dhcp = NETINFO_STATIC };
+
+
+uint8_t destip[4] = {192, 168, 1, 120};
+uint16_t destport = 5000;
+
+
+
 SPI_HandleTypeDef 	hspi2;
 UART_HandleTypeDef  huart2;
+
 
 char msg[60];
 
@@ -49,113 +77,179 @@ static void MX_GPIO_Init(void);
 static void MX_SPI2_Init(void);
 static void MX_USART2_UART_Init(void);
 
-// NSS SHOULD BE ON PINB_12
-void cs_sel() {
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET); //CS LOW
-}
 
-void cs_desel() {
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); //CS HIGH
-}
 
-uint8_t spi_rb(void) {
-	uint8_t rbuf;
-	HAL_SPI_Receive(&hspi2, &rbuf, 1, 0xFFFFFFFF);
-	return rbuf;
-}
+void  wizchip_select(void);
+void  wizchip_deselect(void);
+void  wizchip_write(uint8_t wb);
+uint8_t wizchip_read();
 
-void spi_wb(uint8_t b) {
-	HAL_SPI_Transmit(&hspi2, &b, 1, 0xFFFFFFFF);
-}
+void network_init(void);
 
-/**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  */
+
 int main(void)
 {
-  uint8_t retVal, sockStatus;
-  int16_t rcvLen;
-  uint8_t rcvBuf[20], bufSize[] = {2, 2, 2, 2};
+	   uint8_t tmp = 0;
+	   uint8_t memsize[2][8] = { {2,2,2,2,2,2,2,2},{2,2,2,2,2,2,2,2}};
+
+	   uint8_t retVal, sockStatus;
+	    int16_t rcvLen;
+	     uint8_t rcvBuf[20], bufSize[] = {2, 2, 2, 2};
+
 
   HAL_Init();
 
-  /* Configure the System clock to 84 MHz */
   SystemClock_Config();
 
   MX_GPIO_Init();
   MX_SPI2_Init();
   MX_USART2_UART_Init();
 
-  PRINT_HEADER();
 
-  reg_wizchip_cs_cbfunc(cs_sel, cs_desel);
-  reg_wizchip_spi_cbfunc(spi_rb, spi_wb);
 
-  wizchip_init(bufSize, bufSize);
-  wiz_NetInfo netInfo = { .mac 	= {0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef},	// Mac address
-                          .ip 	= {192, 168, 2, 192},					// IP address
-                          .sn 	= {255, 255, 255, 0},					// Subnet mask
-                          .gw 	= {192, 168, 2, 1}};					// Gateway address
-  wizchip_setnetinfo(&netInfo);
-  wizchip_getnetinfo(&netInfo);
-  PRINT_NETINFO(netInfo);
+  PRINT_STR("Here we START\r\n");
 
-reconnect:
-  /* Open socket 0 as TCP_SOCKET with port 5000 */
-  if((retVal = socket(0, Sn_MR_TCP, 5000, 0)) == 0) {
-	  /* Put socket in LISTEN mode. This means we are creating a TCP server */
-	  if((retVal = listen(0)) == SOCK_OK) {
-		  /* While socket is in LISTEN mode we wait for a remote connection */
-		  while(sockStatus = getSn_SR(0) == SOCK_LISTEN)
-			  HAL_Delay(100);
-		  /* OK. Got a remote peer. Let's send a message to it */
-		  while(1) {
-			  /* If connection is ESTABLISHED with remote peer */
-			  if(sockStatus = getSn_SR(0) == SOCK_ESTABLISHED) {
-				  uint8_t remoteIP[4];
-				  uint16_t remotePort;
-				  /* Retrieving remote peer IP and port number */
-				  getsockopt(0, SO_DESTIP, remoteIP);
-				  getsockopt(0, SO_DESTPORT, (uint8_t*)&remotePort);
-				  sprintf(msg, CONN_ESTABLISHED_MSG, remoteIP[0], remoteIP[1], remoteIP[2], remoteIP[3], remotePort);
-				  PRINT_STR(msg);
-				  /* Let's send a welcome message and closing socket */
-				  if(retVal = send(0, GREETING_MSG, strlen(GREETING_MSG)) == (int16_t)strlen(GREETING_MSG))
-					  PRINT_STR(SENT_MESSAGE_MSG);
-				  else { /* Ops: something went wrong during data transfer */
-					  sprintf(msg, WRONG_RETVAL_MSG, retVal);
-					  PRINT_STR(msg);
-				  }
-				  break;
-			  }
-			  else { /* Something went wrong with remote peer, maybe the connection was closed unexpectedly */
-				  sprintf(msg, WRONG_STATUS_MSG, sockStatus);
-				  PRINT_STR(msg);
-				  break;
-			  }
-		  }
+  wizchip_deselect();
+  reg_wizchip_cs_cbfunc(wizchip_select, wizchip_deselect);
 
-	  } else /* Ops: socket not in LISTEN mode. Something went wrong */
-		  PRINT_STR(LISTEN_ERR_MSG);
-  } else { /* Can't open the socket. This means something is wrong with W5100 configuration: maybe SPI issue? */
-	  sprintf(msg, WRONG_RETVAL_MSG, retVal);
-	  PRINT_STR(msg);
-  }
+  /* SPI read & write callback function */
+  reg_wizchip_spi_cbfunc(wizchip_read, wizchip_write);
 
-  /* We close the socket and start a connection again */
-  disconnect(0);
-  close(0);
-  goto reconnect;
-
-  /* Infinite loop */
-  while (1)
+  /* WIZCHIP SOCKET Buffer initialize */
+  if(ctlwizchip(CW_INIT_WIZCHIP,(void*)memsize) == -1)
   {
+	  PRINT_STR("WIZCHIP Initialized fail.\r\n");
+     while(1);
   }
+  else PRINT_STR("WIZCHIP Initialized success.\r\n");
+  /* PHY link status check */
+
+  do
+  {
+     if(ctlwizchip(CW_GET_PHYLINK, (void*)&tmp) == -1)
+    	 PRINT_STR("Unknown PHY Link stauts.\r\n");
+
+  } while(tmp == PHY_LINK_OFF);
+
+  /* Network Initialization */
+  network_init(); // and print network info
+
+  reconnect:
+  PRINT_STR("Enters loop \r\n");
+    /* Open socket 0 as TCP_SOCKET with port 5000 */
+    if((retVal = socket(0, Sn_MR_TCP, 3000, 0)) == 0) {
+  	  /* Put socket in LISTEN mode. This means we are creating a TCP server */
+    	PRINT_STR("TCP server created,LISTEN MODE \r\n");
+    	if((retVal = listen(0)) == SOCK_OK) {
+    		/* While socket is in LISTEN mode we wait for a remote connection */
+    	PRINT_STR("WAITING FOR REMOTE CNX\r\n");
+  		  while(sockStatus = getSn_SR(0) == SOCK_LISTEN)
+  			  HAL_Delay(100);
+  		  /* OK. Got a remote peer. Let's send a message to it */
+  		  while(1) {
+  			  /* If connection is ESTABLISHED with remote peer */
+  			  if(sockStatus = getSn_SR(0) == SOCK_ESTABLISHED) {
+
+
+  				  PRINT_STR("Cnx established with remote peer\r\n");
+
+  				  uint8_t remoteIP[4];
+  				  uint16_t remotePort;
+  				  /* Retrieving remote peer IP and port number */
+  				  getsockopt(0, SO_DESTIP, remoteIP);
+  				  getsockopt(0, SO_DESTPORT, (uint8_t*)&remotePort);
+  				  sprintf(msg, CONN_ESTABLISHED_MSG, remoteIP[0], remoteIP[1], remoteIP[2], remoteIP[3], remotePort);
+  				  PRINT_STR(msg);
+  				  /* Let's send a welcome message and closing socket */
+  				  if(retVal = send(0, GREETING_MSG, strlen(GREETING_MSG)) == (int16_t)strlen(GREETING_MSG))
+  					  PRINT_STR(SENT_MESSAGE_MSG);
+  				  else { /* Ops: something went wrong during data transfer */
+  					  sprintf(msg, WRONG_RETVAL_MSG, retVal);
+  					  PRINT_STR(msg);
+  				  }
+  				  break;
+  			  }
+  			  else { /* Something went wrong with remote peer, maybe the connection was closed unexpectedly */
+  				  sprintf(msg, WRONG_STATUS_MSG, sockStatus);
+  				  PRINT_STR("cnx was closed\r\n");
+  				  break;
+  			  }
+  		  }
+
+  	  } else /* Ops: socket not in LISTEN mode. Something went wrong */
+  		  PRINT_STR("socket not in LISTEN mode\r\n");
+    } else { /* Can't open the socket. This means something is wrong with W5100 configuration: maybe SPI issue? */
+  	  sprintf(msg, WRONG_RETVAL_MSG, retVal);
+  	  PRINT_STR("Can't open the socket\r\n");
+    }
+
+    /* We close the socket and start a connection again */
+    disconnect(0);
+    close(0);
+    goto reconnect;
+
+    /* Infinite loop */
+    while (1)
+    {
+    }
+
+/*
+   /* Main Loop
+   while(1)
+   {
+	//loopback_tcps(SOCK_TCPS, gDATABUF, PORT_TCPS);
+	loopback_udps(SOCK_UDPS, gDATABUF, PORT_UDPS);
+
+
+	PRINT_STR("here I dont know what is goinf on! \r\n");
+
+	HAL_Delay(5000);
+	//loopback_ret = loopback_tcpc(SOCK_TCPS, gDATABUF, destip, destport);
+	//if(loopback_ret < 0) trace_printf("loopback ret: %ld\r\n", loopback_ret);
+	// TCP Socket
+   }
+
+   */
+
 }
 
 
+void  wizchip_select(void)
+{
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET); //CS LOW
+	}
+
+void  wizchip_deselect(void)
+{
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); //CS HIGH
+}
+
+void  wizchip_write(uint8_t wb)
+{
+	HAL_SPI_Transmit(&hspi2, &wb, 1, 0xFFFFFFFF);
+}
+
+uint8_t wizchip_read()
+{   uint8_t rbuf;
+	HAL_SPI_Receive(&hspi2, &rbuf, 1, 0xFFFFFFFF);
+	return rbuf;
+}
+
+void network_init(void)
+{
+
+
+
+	ctlnetwork(CN_SET_NETINFO, (void*)&gWIZNETINFO);
+
+
+
+    ctlnetwork(CN_GET_NETINFO, (void*)&gWIZNETINFO);
+
+	// Display Network Information
+
+    PRINT_NETINFO(gWIZNETINFO);
+}
 
 
 void SystemClock_Config(void)
